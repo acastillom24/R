b5a26d7e7b009188f1f5a36b3caf3d6c28cfd973
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-10-05 00:00:36 -0500

    infoV02

--- Infographics with R/infographics.R
+++ Infographics with R/infographics.R
@@ -1,3 +1,12 @@
+# Info from:
+# https://www.listendata.com/2019/06/create-infographics-with-r.html?m=1
+
+# Instalacion de paquetes -------------------------------------------------
+# install.packages("devtools")
+# install.packages("waffle")
+# install.packages("remotes")
+
+=======
 # https://www.listendata.com/2019/06/create-infographics-with-r.html?m=1
 
 # Instalación de paquetes -------------------------------------------------
@@ -5,8 +14,12 @@ install.packages("devtools")
 install.packages("waffle")
 install.packages("remotes")
 
+
+# Carga de bibliotecas ---------------------------------------------------- 
 library(dplyr)
 library(ggplot2)
+
+
 
 # Paquete waffle ----------------------------------------------------------
 library(waffle)
@@ -92,4 +105,3 @@ waffle(c(50, 30, 15, 5), rows = 5, use_glyph = "music", glyph_size = 6)
 waffle::fa_grep("female")
 
 devtools::install_github("JohnCoene/echarts4r.assets")
-
